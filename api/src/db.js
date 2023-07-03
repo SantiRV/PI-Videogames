c4e77d01aa0946ad8440426990beba7d4cd35c32
Santiago Rambeaud <santy_r_7@hotmail.com>
2023-07-03 02:23:30 -0300

    Base de datos y relaciones Termindas

--- api/src/db.js
+++ api/src/db.js
@@ -30,11 +30,15 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genere, Platform } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Videogame.belongsToMany(Genere, {through: "videogame_genere"});
+Genere.belongsToMany(Videogame, {through: "videogame_genere"});
 
+Videogame.belongsToMany(Platform, {through: "videogame_platform"});
+Platform.belongsToMany(Videogame, {through: "videogame_platform"});
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
