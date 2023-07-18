5117bff09d8b6870b7adec66ba669f157bc6eaa7
Santiago Rambeaud <santy_r_7@hotmail.com>
2023-07-18 15:11:38 -0300

    Arreglos back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,23 +2,19 @@ const { Router } = require('express');
 const getVideogames = require('./routeVideogames');
 const getGenres = require('./routeGenre')
 const getPlatforms = require('./routePlatform') 
-const cors = require('cors'); // Importa el módulo cors
-const backendURL = "https://pi-videogames-amber-zeta.vercel.app/"
+
 
 const router = Router();
 
-// Configurar CORS solo para las rutas que necesitan peticiones de origen cruzado
-const corsOptions = {
-  origin: '*', // Ajusta esto para permitir peticiones solo desde dominios específicos
-};
+
 
 
 
 
 // Configurar los routers con la URL de tu backend
-router.use(`${backendURL}/videogame`, cors(corsOptions), getVideogames);
-router.use(`${backendURL}/genre`, cors(corsOptions), getGenres);
-router.use(`${backendURL}/platforms`, cors(corsOptions), getPlatforms);
+router.use('/videogame', getVideogames);
+router.use('/genre', getGenres);
+router.use('/platforms', getPlatforms);
 
 module.exports = router;
 
